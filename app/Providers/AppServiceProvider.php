0c4d88c2f47dd70b67c170e45192d7db8296982c
osamamaktari <osamaaltal01@gmail.com>
2025-09-19 21:59:47 +0300

    edit:Appservice logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,28 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+  /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+
+
     /**
      * Register any application services.
      */
@@ -17,8 +35,25 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+  public function boot(): void
     {
-        //
+
+        Gate::define("isAdmin", function (User $user) {
+            return $user->role === "admin";
+        });
+
+        Gate::define("isOrganizer", function (User $user) {
+            return $user->role === "organizer";
+        });
+
+        Gate::define("isAttendee", function (User $user) {
+            return $user->role === "attendee";
+        });
+
+        Gate::define("isOrganizerOrAdmin", function (User $user) {
+            return $user->role === "organizer" || $user->role === "admin";
+        });
     }
 }
+
